6f176886ffe192c990c67afb5043900e48669056
Ajitesh Nair <ajitesh@wirelessprv-10-194-63-121.near.illinois.edu>
2024-02-24 11:55:05 -0600

    use argparse for args

--- HackIllinois/main.py
+++ HackIllinois/main.py
@@ -1,5 +1,12 @@
 import ErrorCatcher as ec
+import argparse
 
-ec.run_file_with_error_catching("Test.py")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run a Linux command')
+    parser.add_argument('--command', metavar='command', type=str, required=True, help='The Linux command to run')
 
-# print("Did not exit the program!")
+    args = parser.parse_args()
+    print(args.command)
+    ec.run_file_with_error_catching(args.command)
+    
+
